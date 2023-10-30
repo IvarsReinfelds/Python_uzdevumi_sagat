40b6a388409e039a62e271398b78c2fff184ba71
Ivars <ivars.reinfelds@grobinasskola.lv>
2023-10-30 15:47:49 +0200

    ja

--- 2uzd.py
+++ 2uzd.py
@@ -1,5 +1,23 @@
 """
 Iegūt ziņas, izmantojot rss no google.lv.
 
-"""
 
+
+
+
+
+"""
+import feedparser
+
+#janorada RSS  plusmas URL
+rss_url='https://news.google.com/rss?hl=en-LV&gl=LV&ceid=LV:lv'
+
+#lejupladet uz izanalizet RSS plusmu
+feed=feedparser.parse(rss_url)
+
+# paradit zinu virsrakstus un saites
+
+for entry in feed.entries:
+    print('virsraksts:', entry.title)
+    print('Saite:', entry.link)
+    print('\n')
