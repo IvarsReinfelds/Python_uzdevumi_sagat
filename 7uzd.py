3ed4fb7916435e2af584f681c22d17bd31b2ac73
Ivars <ivars.reinfelds@grobinasskola.lv>
2023-11-01 13:11:52 +0200

    a

--- 7uzd.py
+++ 7uzd.py
@@ -1,8 +1,17 @@
 """
 Papildināt programmu ar ciklu, kurā sarakstā esošiem uzvārdiem tiktu 
 pievienots klāt doktora nosaukums - Dr.
+
+map()
+
 """
 
 
 saraksts1=["Kalniņš", "Opmanis", "Vēzis", "Almane"]
-sarakts2=[]
+#sarakts2=[]
+
+def pievienot(uzvards):
+    return 'Dr. '+uzvards
+
+saraksts2=list(map(pievienot,saraksts1))
+print(saraksts2)
